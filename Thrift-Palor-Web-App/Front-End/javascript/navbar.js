d37ceeabeb2c87da575f00ca0f0a45fdd8ba8ba9
Inga Mbobo <mboboinga12@gmail.com>
2025-07-18 22:12:17 +0200

    Contact us page

--- Thrift-Palor-Web-App/Front-End/javascript/navbar.js
+++ Thrift-Palor-Web-App/Front-End/javascript/navbar.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
             menuToggle.classList.toggle('open');
             navLinks.classList.toggle('open');
         };
-        // Optional: close menu when a link is clicked (for mobile UX)
         navLinks.querySelectorAll('a').forEach(link => {
             link.addEventListener('click', () => {
                 menuToggle.classList.remove('open');
