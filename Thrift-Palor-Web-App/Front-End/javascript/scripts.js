db8f55b0fa10f4efc02893dd80135df077f0b99d
Inga Mbobo <mboboinga12@gmail.com>
2025-07-23 15:20:23 +0200

    Images

--- Thrift-Palor-Web-App/Front-End/javascript/scripts.js
+++
@@ -1,6 +0,0 @@
- document.addEventListener('DOMContentLoaded', function() {
-            if (localStorage.getItem('thriftpalor_user')) {
-                document.getElementById('auth-wall').style.display = 'none';
-                document.getElementById('app-content').style.display = 'block';
-            }
-        });
