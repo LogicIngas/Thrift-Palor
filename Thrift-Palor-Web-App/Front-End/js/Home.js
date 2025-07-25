20b6f34862b8cc10c6079aa9834640fb29ad2ceb
Inga Mbobo <mboboinga12@gmail.com>
2025-07-25 22:59:00 +0200

    Add Images tomorrow

--- Thrift-Palor-Web-App/Front-End/js/Home.js
+++ Thrift-Palor-Web-App/Front-End/js/Home.js
@@ -1,15 +1,15 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const bar = document.getElementById('bar');  
+const close = document.getElementById('close');  
+const nav = document.getElementById('navbar');  
 
-if(bar){
-    bar.addEventListener('clcik', () => {
+if (bar) {
+    bar.addEventListener('click', () => {  
         nav.classList.add('active');
-    })
+    });
 }
 
-if(close){
+if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
-    })
+    });
 }
